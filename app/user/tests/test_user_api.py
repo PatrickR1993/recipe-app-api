90a68653640925f1fa741dbd0ac8030dbd9b8ca4
Patrick Ruizendaal <patrick_ruizendaal@hotmail.com>
2023-07-17 14:29:29 +0200

    Fixed Linting

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -60,7 +60,9 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload["email"]
+        ).exists()
         self.assertFalse(user_exists)
 
     def create_token_for_user(self):
@@ -72,7 +74,10 @@ class PublicUserApiTests(TestCase):
         }
         create_user(**user_details)
 
-        payload = {"email": user_details["email"], "password": user_details["password"]}
+        payload = {
+            "email": user_details["email"],
+            "password": user_details["password"]
+            }
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertIn("token", res.data)
