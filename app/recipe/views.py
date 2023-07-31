29cf4ebb9ec1467ed0c1039d80cca9e70133305a
Patrick Ruizendaal <patrick_ruizendaal@hotmail.com>
2023-07-31 16:23:22 +0200

    Fixed bug

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -114,7 +114,7 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
         assigned_only = bool(
             int(self.request.query_params.get('assigned_only', 0))
         )
-        queryset = self.get_queryset
+        queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
 
